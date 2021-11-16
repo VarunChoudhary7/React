783a7ba90ba16eb6b69f591f48254e3d9e9d2108
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-11-16 18:44:49 +0530

    First commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,16 @@
 
-function App() {
+import Navbar from './Navbar'
+
+const App = () => {
   return (
     <div className="App">
+
+      <Navbar />
+
       Hello i am a waste text
       <h1>I am h1</h1>
+      <h1>I am h1</h1>
+
     </div>
   );
 }
