2da0bf981ac4653c3338604deedab8932a529cee
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-11-19 21:42:01 +0530

    4th 2nd

--- myapp/src/Components/Content.js
+++ myapp/src/Components/Content.js
@@ -1,55 +1,33 @@
-import ListItem from "./ListItem";
 import { useState } from 'react'
-import { render } from 'react-dom'
 
 const Content = () => {
 
-    // let counter = 0
-    // let counter = useState(0)
-    // const [counter, mysteryFunction] = useState({})
-    // let [counter, mysteryFunction] = useState(0)
-    let [counter, setCounter] = useState(0)
+    const [items, setItems] = useState([])
+    const [value, setValue] = useState('')
 
+    const handleSubmit = () => {
+        setItems(prev => [...prev, value])
+        setValue('')
+    }
 
-    // in function component we can't use state natively so we use this work around called useState while in class component we can access state directly
-    // console.log(counter)
-    // console.log(mysteryFunction)
+    const handleInput = (e) => {
+        setValue(e.target.value)
+    }
 
-
+    const handleDelete = (item) => {
+        console.log(item)
+        setItems(prev => prev.filter(i => i !== item))
+    }
     return (
-        <div className="container">
-            <h3>{counter}</h3>
-            <button onClick={() => {
-                // console.log(counter)
-                // counter = counter + 1
-                // mysteryFunction(previousValue => {
-                setCounter(previousValue => {
-
-                    console.log(previousValue)
-                    return previousValue + 1
-                })
-            }}>ClickMe</button>
-        </div>
+        <>
+            <h2>Todo</h2>
+            <input value={value} onChange={handleInput} type="text" />
+            <button onClick={handleSubmit} type="submit">Add</button>
+            <ul>
+                {items.map(item => <li>{item}<button onClick={() => { handleDelete(item) }}>Delete</button></li>)}
+            </ul>
+        </>
     );
 }
 
 export default Content;
-
-// import React from "react";
-// class ClassComponent extends React.Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//             count: 0
-//         }
-//     }
-//     render() {
-//         return (
-//             <div className="container">
-//                 Hello I am waste text
-//             </div>
-//         )
-//     }
-// }
-
-// export default ClassComponent
